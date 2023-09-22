9617900354a6345d849a82e6eaa1903196074582
Miguel Ángel Lombana García <84014018+miguellombana@users.noreply.github.com>
2023-09-22 12:36:34 +0100

    Primer commit

---
+++ src/main/java/dad/Main.java
@@ -0,0 +1,10 @@
+package dad;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+   int solution(int param1, int param2){
+       return param1 + param2;
+   }
+
+}
